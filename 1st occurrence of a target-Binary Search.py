59e3921fbfbb2f11372bdfcf9232890e06c10058
kothuri Gayathri <137641378+Gayathri10000@users.noreply.github.com>
2024-06-11 20:20:58 +0530

    Create 1st occurrence of a target-Binary Search.py

---
+++ 1st occurrence of a target-Binary Search.py
@@ -0,0 +1,21 @@
+ # First occurrence of a target using Binary Search
+
+def findFirstOccurrence(nums, target):
+    n = len(nums)
+    left, right = 0, n - 1 
+    result = -1 
+    while left <= right:
+        mid = (left + right) // 2 
+        if nums[mid] == target:
+            result = mid
+            
+            right = mid - 1
+        elif nums[mid] > target:
+            right = mid - 1 
+        else:
+            left = mid + 1 
+ 
+    return result
+nums = [1,4,4,4,5,2,2,7,8]
+print(findFirstOccurrence(nums, 4))
+print(findFirstOccurrence(nums, 2))
